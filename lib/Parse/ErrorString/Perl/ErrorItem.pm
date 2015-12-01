c58821e62f7caf364783fa43b125dcdf616523f1
Gabor Szabo <gabor@szabgab.com>
2015-12-01 23:11:51 +0200

    version to 0.22

--- lib/Parse/ErrorString/Perl/ErrorItem.pm
+++ lib/Parse/ErrorString/Perl/ErrorItem.pm
@@ -4,7 +4,7 @@ package Parse::ErrorString::Perl::ErrorItem;
 use strict;
 use warnings;
 
-our $VERSION = '0.21';
+our $VERSION = '0.22';
 
 use Class::XSAccessor getters => {
 	type             => 'type',
