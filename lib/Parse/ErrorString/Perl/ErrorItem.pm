2a952235b9b7907179c68a2370cd7b16dd103b0e
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2017-06-23 08:50:36 +0100

    * Merged PR #5, thanks @knileuh.

--- lib/Parse/ErrorString/Perl/ErrorItem.pm
+++ lib/Parse/ErrorString/Perl/ErrorItem.pm
@@ -4,7 +4,7 @@ package Parse::ErrorString::Perl::ErrorItem;
 use strict;
 use warnings;
 
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 use Class::XSAccessor getters => {
 	type             => 'type',
